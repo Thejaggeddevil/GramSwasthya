96dbc2f88f793d96dee9be71806fd35be00a44fb
ayutanu <ayutanu1230@gmail.com>
2025-06-30 15:25:49 +0530

    build files changed

--- gram_seva/lib/main_production.dart
+++ gram_seva/lib/main_production.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'screens/splash_screen.dart';
 import 'main.dart';
