01945ebb698fc38d76166c48d2f43d07c8eecafa
Arjun Ingole <arjuningole38@gmail.com>
2025-07-07 23:46:03 +0530

    [FIX] regenerate the gradle config and use a updated version of telephony pkg
    Signed-off-by: Arjun Ingole <arjuningole38@gmail.com>

--- gram_seva/lib/utils/sms_sender.dart
+++ gram_seva/lib/utils/sms_sender.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:telephony/telephony.dart';
+import 'package:another_telephony/telephony.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:geolocator/geolocator.dart';
